47ad3f4292e8dfb8358ba9a77b8ba552f36aeef7
Anthony <anthonymellon@yahoo.com>
2025-07-17 11:40:02 +1200

    UI_Panel works with new camera drawing

--- DungeonCrawler/Code/UI/UI_Panel.cs
+++ DungeonCrawler/Code/UI/UI_Panel.cs
@@ -1,36 +1,49 @@
-﻿using Microsoft.Xna.Framework;
+﻿using DungeonCrawler.Code.Utils;
+using DungeonCrawler.Code.Utils.Drawables;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using DungeonCrawler.Code.Utils;
-using System.Collections.Generic;
 
 namespace DungeonCrawler.Code.UI
 {
     internal class UI_Panel : UIComponent
     {
-        private Texture2D _texture;
-        private Color? _color;
+        public UI_Panel(
+            Vector4 anchorPoints,
+            Point4 padding,
+            Point offset,
+            Texture2D texture,
+            Color color) :
+            base(anchorPoints, padding, offset)
+        {
+            _backgroundSprite = new DrawableSprite(texture, ScreenRectangle, color, GameConstants.GameLayers.UI);
+        }
 
         public UI_Panel(
             Vector4 anchorPoints,
             Point4 padding,
             Point offset,
-            Texture2D texture = null,
-            Color? color = null) :
+            Texture2D texture) :
             base(anchorPoints, padding, offset)
         {
-            _texture = texture;
-            _color = color;
+            _backgroundSprite = new DrawableSprite(texture, ScreenRectangle, Color.White, GameConstants.GameLayers.UI);
         }
+
+        public UI_Panel(
+            Vector4 anchorPoints,
+            Point4 padding,
+            Point offset) :
+            base(anchorPoints, padding, offset)
+        {
+            _backgroundSprite = null;
+        }
+
+        private DrawableSprite _backgroundSprite;
 
         protected override void Draw(GameTime gametime, Camera camera)
         {
-            // only draw if there is a texture to draw, this allows for invisible panels
-            if (_texture != null)
+            if (_backgroundSprite != null)
             {
-                // Default to white color if none is given
-                Color color = _color != null ? _color.Value : Color.White;
-
-                graphics.Draw(_texture, ScreenRectangle, color);                
+                camera.DrawSprite(_backgroundSprite);
             }
         }
     }
