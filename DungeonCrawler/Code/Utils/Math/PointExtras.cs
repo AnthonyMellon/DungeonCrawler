ab6c28d021c06cbaa51533eeb3f96339737e6299
AnthonysLaptop\Anthony <anthonymellon@yahoo.com>
2025-07-22 11:42:49 +1200

    Actually fix right move vector + fix up / down move vector

--- DungeonCrawler/Code/Utils/Math/PointExtras.cs
+++ DungeonCrawler/Code/Utils/Math/PointExtras.cs
@@ -9,10 +9,10 @@ namespace DungeonCrawler.Code.Utils.Math
 {
     internal static class PointExtras
     {
-        public static Point Up => new Point(0, 1);
-        public static Point Down => new Point(0, -1);
+        public static Point Up => new Point(0, -1);
+        public static Point Down => new Point(0, 1);
         public static Point Left => new Point(-1, 0);
-        public static Point Right => new Point(0, 1);
+        public static Point Right => new Point(1, 0);
 
         public static float Distance(Point point1, Point point2)
         {
