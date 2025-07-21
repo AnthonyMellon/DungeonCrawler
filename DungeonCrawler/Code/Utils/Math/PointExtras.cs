967f418baed7abd3f974b8c862ee3d105cbfa3ce
AnthonysLaptop\Anthony <anthonymellon@yahoo.com>
2025-07-22 11:40:36 +1200

    Fix right move vector

--- DungeonCrawler/Code/Utils/Math/PointExtras.cs
+++ DungeonCrawler/Code/Utils/Math/PointExtras.cs
@@ -12,7 +12,7 @@ namespace DungeonCrawler.Code.Utils.Math
         public static Point Up => new Point(0, 1);
         public static Point Down => new Point(0, -1);
         public static Point Left => new Point(-1, 0);
-        public static Point Right => new Point(1, 1);
+        public static Point Right => new Point(0, 1);
 
         public static float Distance(Point point1, Point point2)
         {
