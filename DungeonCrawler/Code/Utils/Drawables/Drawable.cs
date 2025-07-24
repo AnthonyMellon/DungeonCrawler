d3bb449e7a5feb5faf14f8a95de8f6fa7fa58813
AnthonysLaptop\Anthony <anthonymellon@yahoo.com>
2025-07-25 09:09:30 +1200

    Add Drawable class, this is the base class for anything that wants to be drawn to the screen

---
+++ DungeonCrawler/Code/Utils/Drawables/Drawable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace DungeonCrawler.Code.Utils.Drawables
+{
+    internal abstract class Drawable
+    {
+        protected Color Color { get; set; }
+        private DrawManager.DrawTargets _drawTarget { get; set; }
+
+        public Action OnChange;
+        public bool Visible
+        {
+            get
+            {
+                return _visible;
+            }
+            set
+            {
+                if (_visible) DrawManager.RegisterDrawable(_drawTarget, this);
+                if (!_visible) DrawManager.DeregisterDrawable(_drawTarget, this);
+
+                _visible = value;
+            }
+        }
+        private bool _visible;
+
+        public abstract void Draw(SpriteBatch spritebatch, GameTime gameTime);
+
+        public Drawable(DrawManager.DrawTargets drawTarget, bool visible = true)
+        {
+            _drawTarget = drawTarget;
+            Visible = visible;
+        }
+    }
+}
