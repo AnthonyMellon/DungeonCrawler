2926ce18be2ec92e9f0875c90123f507cad018b0
AnthonyMellon_ <anthonymellon@yahoo.com>
2025-08-04 21:33:27 +1200

    Starting to try serialize Rooms into XML - arrays are not wanting to parse :(

---
+++ DungeonCrawler/Code/Data/RoomData.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Serialization;
+
+namespace DungeonCrawler.Code.Data
+{
+    public class RoomData
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int[] Tiles { get; set; }
+    }
+}
