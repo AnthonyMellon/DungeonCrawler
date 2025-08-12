84655e9a5eb59604a9ffb565fc3c2f15d1920a73
AnthonysLaptop\Anthony <anthonymellon@yahoo.com>
2025-08-12 14:09:46 +1200

    Create xml data for spritesheets. not being used yet

---
+++ DungeonCrawler/Code/Data/SpriteSheetData.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawler.Code.Data
+{
+    public class SpriteSheetData
+    {
+        public string Path { get; set; }
+        public Dictionary<string, Rectangle> SpriteRectangles { get; set; }
+    }    
+}
