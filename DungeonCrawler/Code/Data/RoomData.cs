8ddee185a2ee8bdca8f4666850ad8f42b83c138e
AnthonyMellon_ <anthonymellon@yahoo.com>
2025-09-04 15:46:56 +1200

    Remove width and height from roomData

--- DungeonCrawler/Code/Data/RoomData.cs
+++ DungeonCrawler/Code/Data/RoomData.cs
@@ -6,8 +6,6 @@ namespace DungeonCrawler.Code.Data
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public int Width { get; set; }
-        public int Height { get; set; }
         public List<List<int>> Tiles { get; set; }
     }
 }
